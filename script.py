b974f3a8eca694d6b4a004a6da3c774a3092b3de
mfazin <78682897+mfazin@users.noreply.github.com>
2021-02-11 17:28:08 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,7 +2,24 @@ class script(object):
     START_TEXT = """ A Simple And Fastest File Renamer Bot With Permanent Thumbnail support!💯
 
 <b>Send me any Telegram file and choose appropriate option! </b>
-For Know More Hit /help."""
+For Know More Hit /help.
+@pyrogram.Client.on_message(pyrogram.Filters.command(["help"]))
+async def help_user(bot, update):
+
+#   TRChatBase(update.from_user.id, update.text, "/help")
+    buttons = [[
+        InlineKeyboardButton('🆘 Help 🆘', callback_data='help'),
+        InlineKeyboardButton('Upgrade💸', callback_data='upgrade_btn')
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.HELP_USER,
+        reply_markup=reply_markup,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )"""
 
     RENAME_403_ERR = "What Are You Doing? You are Banned"
     UPGRADE_TEXT = "CONTACT @Ek_comme_nt_bot"
@@ -17,6 +34,6 @@ For Know More Hit /help."""
     HELP_USER = """It's not that complicated😅
     
 <b>1. Send me any Telegram File.
-   2. Choose appropriate option.
+2. Choose appropriate option.
    Enjoy😍.</b>"""
 
