16e903251144013566602c35f5765405ecb97a1e
mfazin <78682897+mfazin@users.noreply.github.com>
2021-02-11 10:58:17 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,21 +1,22 @@
 class script(object):
-    START_TEXT = """ A Simple File Renamer Bot With Permanent Thumbnail support!💯
+    START_TEXT = """ A Simple And Fastest File Renamer Bot With Permanent Thumbnail support!💯
 
-<b>Send me any Telegram file and choose appropriate option! </b>"""
+<b>Send me any Telegram file and choose appropriate option! </b>
+For Know More Hit /help."""
 
     RENAME_403_ERR = "What Are You Doing? You are Banned"
     UPGRADE_TEXT = "CONTACT @Ek_comme_nt_bot"
     DOWNLOAD_START = "Give Me Some Time 🙏..."
     UPLOAD_START = "Starting to upload 😝..."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me. @ekbotz_update.**"
-    SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
+    SAVED_THUMB = "Thumbnail Saved as permanent ✅ This Is Permanent"
     DEL_THUMB = "Thumbnail cleared succesfully!"
     NO_THUMB = "No thumbnails found!😐"
     SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
     CUSTOM_CAPTION_UL_FILE = " "
     HELP_USER = """It's not that complicated😅
     
-1. Send me any Telegram File.
-2. Choose appropriate option.
-   <b>Enjoy😍.</b>"""
+<b>1. Send me any Telegram File.
+   2. Choose appropriate option.
+   Enjoy😍.</b>"""
 
