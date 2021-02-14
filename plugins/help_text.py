4009d476f264e0ff8be529ae5b8fe9ded07a6728
mfazin <78682897+mfazin@users.noreply.github.com>
2021-02-14 07:31:32 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -45,8 +45,6 @@ def send_start(bot, update):
     bot.send_message(
         chat_id=update.chat.id,
         text=script.START_TEXT.format(update.from_user.first_name),
-        reply_markup=Inlinekeyboardmarkup([[InlineKeyboardButton(text="🔱 Channel 🔱", url="https://t.me/ekbotz_update")]]),
-        parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
     )
