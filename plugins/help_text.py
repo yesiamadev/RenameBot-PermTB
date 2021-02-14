2bd41cf9c9527c9c1ab02d62d8399b2662adfa33
yesiamadev <77689908+yesiamadev@users.noreply.github.com>
2021-02-14 20:18:05 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -46,8 +46,12 @@ def send_start(bot, update):
     bot.send_message(
         chat_id=update.chat.id,
         text=script.START_TEXT.format(update.from_user.first_name),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🔰 Channel 🔰", url="https://t.me/ekbotz_update")],
+                          [InlineKeyboardButton(text="♐️ Share ♐️", url="https://t.me/share/url?url=t.me/Rename_EK_Bot")],
+                                 
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
+        
     )
 
 
